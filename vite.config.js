08192b1c926fef3e796aa29604e2de2c601db47d
Keerthana M <keerthanamanjunatha1234@gmail.com>
2025-12-09 19:07:43 +0530

    Fix GitHub Pages Build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioWithReact/',   // IMPORTANT for GitHub Pages
+  base: '/PortfolioWithReact/', // MUST MATCH REPO NAME EXACTLY
 })
