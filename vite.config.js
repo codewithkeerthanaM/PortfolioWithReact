46d5bcd8305ae62c27ba1d9c5aec6049cdb1f8bc
Keerthana M <keerthanamanjunatha1234@gmail.com>
2025-12-09 19:56:32 +0530

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortfolioWithReact/',   // 👈 VERY IMPORTANT
+  base: '/PortfolioWithReact/',
 })
+
